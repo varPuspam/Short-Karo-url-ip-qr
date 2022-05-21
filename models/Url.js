bfc7ba8748faf7ab3ae3c48447e69f114beb4c94
varPuspam <pushpamthakur111@gmail.com>
2022-05-21 13:18:52 +0530

    init

---
+++ models/Url.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: "ID is required!"
+    },
+    url: {
+        type: String,
+        required: "URL is required!"
+    },
+    visitors: {
+        type: Number,
+        required: "Visits on this url!"
+    },
+    ip:[{
+        type:String
+    }]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Url', urlSchema);
